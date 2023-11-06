1e92d5b1b72fe80819b8fd50256e1753cea10b5e
R-2PO <r2po@duck.com>
2023-11-06 01:53:54 +0100

    Add doc to sockutils.recv_until_end

--- sockutils.py
+++ sockutils.py
@@ -1,6 +1,15 @@
 import socket
 
 def recv_until_end (sock: socket.socket) -> bytes:
+    """
+    Same as socket.recv, but reads until nothing else is sent
+
+    Arguments:
+        -sock: the socket to read data from
+
+    Returns:
+        The received data
+    """
     data = b""
     new_data = b"aaaaaaaaaa"  #Length of 10 to start the loop
     while len(new_data) == 10:
