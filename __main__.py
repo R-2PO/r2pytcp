5db6417008360ed562455a9e3a6f04f60116e3ef
R-2PO <95053976+R-2PO@users.noreply.github.com>
2024-07-02 14:57:58 +0200

    Create custom handler in __main__.py

--- __main__.py
+++ __main__.py
@@ -1,7 +1,12 @@
 from r2pytcp import *
 
+class Handler (HTTPHandler):
+    def __init__ (self, client_socket):
+        super().__init__(client_socket)
+        self.close()
+
 if __name__ == "__main__":
-    server = TCPServer(("0.0.0.0", 3500), HTTPHandler, allow_reuse_port=True)
+    server = TCPServer(("0.0.0.0", 3500), Handler, allow_reuse_port=True)
     try:
         server.run()
     except KeyboardInterrupt:
